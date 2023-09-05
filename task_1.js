c793bb182aa608aba97b230e30700f7ff89b53d2
Matsvei Khmialnitski <geniusx1990@gmail.com>
2023-09-05 19:15:30 +0300

    fix: updated flow

--- task_1.js
+++ task_1.js
@@ -35,15 +35,17 @@ manager.scheduleImmediate();
 
 
 /*  Expected Execution Flow:
-1. First simulateAsyncOperation with a 200 ms delay is scheduled.
-2. Second simulateAsyncOperation with a 1000 ms delay is scheduled.
-3. The promise task is scheduled.
-4. The immediate task is scheduled.
-5. The event loop begins:
-6. The first simulateAsyncOperation completes after 200 ms.
-7. The corresponding microtask is executed immediately.
-8. The second simulateAsyncOperation completes after 1000 ms.
-9. Its microtask is executed immediately.
-10. The immediate task is executed.
-11. The program ends.
+1. Instance of the "AsyncOperationManager" class is created.
+2. First simulateAsyncOperation with a 200 ms delay is scheduled and registered a callback function. 
+3. The microtask using Promise is scheduled and executed.
+4. Second simulateAsyncOperation with a 1000 ms delay is scheduled and registered a callback function
+5. The immediate task is scheduled and registered a callback function.
+
+The event loop begins:
+6. The immediate task is executed.
+7. The first simulateAsyncOperation completed after 200 ms and the callback is executed.
+8. The microtask process.nextTick is executed immediately.
+9. The second simulateAsyncOperation completed after 1000 ms and the callback is executed.
+10. The microtask process.nextTick is executed immediately.
+12. The program ends.
  */
